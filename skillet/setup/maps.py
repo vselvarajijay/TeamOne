a3d341d0ebf8873810e683eed1e9dd068700b575
Ali Kuwajerwala <ali@kscale.dev>
2025-01-18 04:39:45 -0800

    script to configure and zero actuators

---
+++ skillet/setup/maps.py
@@ -0,0 +1,24 @@
+"""Maps actuator names to their IDs."""
+
+ACTUATOR_NAME_TO_ID = {
+    "left_shoulder_yaw": 11,
+    "left_shoulder_pitch": 12,
+    "left_elbow_yaw": 13,
+    "left_gripper": 14,
+    "right_shoulder_yaw": 21,
+    "right_shoulder_pitch": 22,
+    "right_elbow_yaw": 23,
+    "right_gripper": 24,
+    "left_hip_yaw": 31,
+    "left_hip_roll": 32,
+    "left_hip_pitch": 33,
+    "left_knee_pitch": 34,
+    "left_ankle_pitch": 35,
+    "right_hip_yaw": 41,
+    "right_hip_roll": 42,
+    "right_hip_pitch": 43,
+    "right_knee_pitch": 44,
+    "right_ankle_pitch": 45,
+}
+
+ACTUATOR_ID_TO_NAME = {v: k for k, v in ACTUATOR_NAME_TO_ID.items()}
---
+++ skillet/setup/setup_zeroing.py
@@ -0,0 +1,45 @@
+"""Script to configure and zero actuators in the KOS system."""
+
+# Standard library imports
+import logging
+import time
+import traceback
+
+# Third-party imports
+import colorlogging
+import pykos
+
+# Local imports
+from skillet.setup.maps import ACTUATOR_NAME_TO_ID
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def main() -> None:
+    """Configure and zero actuators, reading their states."""
+    colorlogging.configure()
+    kos = pykos.KOS(ip="192.168.42.1")
+
+    for actuator_name, actuator_id in ACTUATOR_NAME_TO_ID.items():
+        try:
+            # Configure actuator
+            logger.info("Configuring actuator %s with id %d to zero position", actuator_name, actuator_id)
+            result = kos.actuator.configure_actuator(actuator_id=actuator_id, zero_position=True)
+            logger.info("Result for actuator %s with id %d: %s", actuator_name, actuator_id, result)
+
+            breakpoint()
+            # Get and log actuator state
+            state = kos.actuator.get_actuators_state([actuator_id])
+            logger.info("Current state for actuator %s with id %d: %s", actuator_name, actuator_id, state)
+
+            time.sleep(0.1)
+        except Exception as e:
+            logger.error(
+                "Error while configuring/checking actuator %s (ID: %d): %s", actuator_name, actuator_id, str(e)
+            )
+            logger.error("Traceback:\n%s", traceback.format_exc())
+
+
+if __name__ == "__main__":
+    main()
